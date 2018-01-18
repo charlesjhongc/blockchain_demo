fb8242d7f6215eba529b6db252e098ea57492179
Charles Jhong <shad0356jc@gmail.com>
2018-01-18 12:35:16 +0800

    Implement blockchain layout

--- server.py
+++ server.py
@@ -2,11 +2,21 @@ from flask import Flask, render_template, request, redirect
 import json
 
 app = Flask(__name__)
-block = {'BlockHasg': 1234, 'PrevHash':0000, 'BlockHeight':1, 'TXs':[{'Hash':1100, 'From':1234, 'To':2345, 'Value':5000}, {'Hash':9887, 'From':1424, 'To':5566, 'Value':87}]}
+blockchain = []
+genesisBlock = {'BlockHasg': 1234, 'PrevHash':0000, 'BlockHeight':1, 'TXs':[{'Hash':1100, 'From':1234, 'To':2345, 'Value':5000}, {'Hash':9887, 'From':1424, 'To':5566, 'Value':87}]}
+blockchain.append(genesisBlock)
 
 @app.route("/")
 def mainPage():
-    return render_template('index.html', block = block)
+    return render_template('index.html', blockchain = blockchain)
+
+def newBlock():
+    block = {}
+    block['BlockHasg'] = 7878
+    block['PrevHash'] = 8787
+    block['BlockHeight'] = len(blockchain)+1
+    blockchain.append(block)
+    return
 
 if __name__ == '__main__':
     app.debug = True
